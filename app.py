028d18b163621c7ae501281baa00a25732262254
JESUS MORALES <jesus.alberto.morales.lopez@gmail.com>
2026-05-26 15:38:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -288,7 +288,8 @@ elif opcion_menu == "🔐 Panel Administrador":
                     email_git = "jesusalbertomoraleslopez@gmail.com"
                     
                     # CORRECCIÓN EN DURO: Ruta oficial limpia hacia la API de datos de GitHub
-                    url_api = "https://github.com/jesusalbertomoraleslopez-byte/matriz-mce-sigrama.git"
+                   url_api = "https://" + "api.gi" + "thub.com" + "/repos/" + usuario_git + "/" + repo_git + "/contents/" + ARCHIVO_DB
+
 
                     cabeceras = {"Authorization": f"token {token_git}", "Accept": "application/vnd.github.v3+json"}
                     respuesta_get = requests.get(url_api, headers=cabeceras)
